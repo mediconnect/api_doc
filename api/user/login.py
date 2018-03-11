4faafde09c861a6f08aa913cce437e74f3c6211f
Jiashen Cao <caojiashen24@gmail.com>
2018-03-11 18:17:24 -0400

    initialize with first sample api

--- api/user/login.py
+++ api/user/login.py
@@ -2,4 +2,10 @@
 @api {post} /user/login/
 @apiParam {String} email Mandatory email address.
 @apiParam {String} password Mandatory password.
+
+@apiName User login
+@apiDescription Handle user login request.
+@apiGroup User
+
+@apiVersion 0.0.0
 """

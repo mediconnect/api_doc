3cecc28a6cb5aec05486d6a148692758fe482e9b
Jiashen Cao <caojiashen24@gmail.com>
2018-03-11 18:27:37 -0400

    clean repos

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-03-11T22:16:57.215Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-03-11T22:26:55.074Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});

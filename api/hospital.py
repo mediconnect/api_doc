640f4f05cb02bde9450f1f1c3b1747965828eec9
unknown <runye.wang@emory.edu>
2018-03-12 12:45:22 -0400

    hospital display api

---
+++ api/hospital.py
@@ -0,0 +1,10 @@
+"""
+@api {get} /hospital/
+@apiParam {id} hospital_id
+
+@apiName Hospital Display
+@apiDescription Display information about a hospital.
+@apiGroup Hospital
+
+@apiVersion 0.0.0
+"""

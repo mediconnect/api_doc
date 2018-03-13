3e96faaef305bca8e553fc10b2b0648d5c1de41b
Arnoldosmium <eaglemewpsy@gmail.com>
2018-03-13 04:53:01 +0100

    patient module edit

--- api/patient.py
+++ api/patient.py
@@ -1,5 +1,5 @@
 """
-@api {post} /patient/:user_id/create Create patient
+@api {post} /patient/create Create patient
 @apiName CreatePatient
 @apiGroup Patient
 
@@ -8,35 +8,51 @@
 @apiParam {String}   last_name			Mandatory last name.
 @apiParam {String}   first_name_pinyin	Mandatory first name pinyin.
 @apiParam {String}   last_name_pinyin	Mandatory last name pinyin.
-@apiParam {String}   gender="M"			Mandatory gender with default "M".
+@apiParam {String}   gender				Mandatory gender with default "M".
 @apiParam {DateTime} birthdate			Mandatory birthdate.
 @apiParam {String}   relationship		Mandatory replationship with user.
 @apiParam {String}	 passport_number 	Mandatory passport number.
-@apiParam {String}	 note				Optional note.
+@apiParam {String}	 [note]				Optional note.
 
-@apiSuccess {String} msg		Successfully create patient. 
+@apiSuccess {String} patient_id			On Successful creation, return patient_id. 
 
 @apiVersion 0.0.0
 """
 
 """
-@api {post} /patient/:user_id/update Update patient
+@api {post} /patient/:patient_id/update Update patient
 @apiName UpdatePatient
 @apiGroup Patient
 
-@apiParam {Number}   user_id			Mandatory unique User ID.
-@apiParam {Number}	 patient_id			Mandatory unique Patient ID.
-@apiParam {String}   first_name 		Mandatory first name.
-@apiParam {String}   last_name			Mandatory last name.
-@apiParam {String}   first_name_pinyin	Mandatory first name pinyin.
-@apiParam {String}   last_name_pinyin	Mandatory last name pinyin.
-@apiParam {String}   gender="M"			Mandatory gender with default "M".
-@apiParam {DateTime} birthdate			Mandatory birthdate.
-@apiParam {String}   relationship		Mandatory replationship with user.
-@apiParam {String}	 passport_number 	Mandatory passport number.
-@apiParam {String}	 note				Optional note.
+@apiParam {Number}	 patient_id				Mandatory unique Patient ID.
+@apiParam {String}   [first_name]	 		Mandatory first name.
+@apiParam {String}   [last_name]			Mandatory last name.
+@apiParam {String}   [first_name_pinyin]	Mandatory first name pinyin.
+@apiParam {String}   [last_name_pinyin]		Mandatory last name pinyin.
+@apiParam {String}   [gender]				Mandatory gender with default "M".
+@apiParam {DateTime} [birthdate]			Mandatory birthdate.
+@apiParam {String}   [relationship]			Mandatory replationship with user.
+@apiParam {String}	 [passport_number] 		Mandatory passport number.
+@apiParam {String}	 [note]					Optional note.
 
 @apiSuccess {String} msg		Successfully update patient. 
+
+@apiVersion 0.0.0
+"""
+
+"""
+@api {post} /patient/:patient_id/visit_history/add	Create patient previous hospital visit history
+@apiName UpdatePatient
+@apiGroup Patient
+
+@apiParam {Number}	 patient_id				Mandatory unique Patient ID.
+@apiParam {String}   hospital_name	 		Mandatory hospital visited.
+@apiParam {String}   doctor_name			Mandatory doctor consulted.
+@apiParam {String}   contact_info			Mandatory doctor contact info.
+@apiParam {String}   disease_category		Mandatory main disease category for the visit.
+@apiParam {DateTime} [visit_date]			Optional visit date.
+
+@apiSuccess {String} visit_id		On success, return visit history id.
 
 @apiVersion 0.0.0
 """
@@ -52,7 +68,7 @@
 @apiSuccess {String}    last_name			Mandatory last name.
 @apiSuccess {String}    first_name_pinyin	Mandatory first name pinyin.
 @apiSuccess {String}    last_name_pinyin	Mandatory last name pinyin.
-@apiSuccess {String}    gender="M"			Mandatory gender with default "M".
+@apiSuccess {String}    gender				Mandatory gender with default "M".
 @apiSuccess {DateTime}  birthdate			Mandatory birthdate.
 @apiSuccess {String}    relationship		Mandatory replationship with user.
 @apiSuccess {String}	passport_number 	Mandatory passport number.

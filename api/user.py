6ccfec9c3ce92e32418336fe21348f018791ad2e
Jiashen Cao <caojiashen24@gmail.com>
2018-03-13 00:51:21 -0400

    add user registration api doc

--- api/user.py
+++ api/user.py
@@ -16,20 +16,42 @@
 @apiDescription Request user infomation.
 @apiGroup User
 
-@apiParam {Number}	 id	 		Mandatory unique User ID.
+@apiParam {Number} id Mandatory unique User ID.
 
-@apiSuccess {Object} user 		User Object.
-@apiSuccess	{String} user.first_name	Mandatory User first name.
-@apiSuccess	{String} user.last_name		Mandatory User last name.
-@apiSuccess	{Number} user.age			Optional User age.
-@apiSuccess {String} user.address		Mandatory User address.
-@apiSuccess {String} user.email 		Mandatory User email.
-@apiSuccess {String} user.phone 		Mandatory User phone.
-@apiSuccess	{String} user.wechat		Optional User wechat.
-@apiSuccess {String} user.qq 			Optional User qq. 
-@apiSuccess	{String} user.gender		Optional User gender.
+@apiSuccess {Object} user User Object.
+@apiSuccess {String} user.first_name Mandatory User first name.
+@apiSuccess {String} user.last_name Mandatory User last name.
+@apiSuccess {Number} user.age Optional User age.
+@apiSuccess {String} user.address Mandatory User address.
+@apiSuccess {String} user.email Mandatory User email.
+@apiSuccess {String} user.phone Mandatory User phone.
+@apiSuccess {String} user.wechat Optional User wechat.
+@apiSuccess {String} user.qq Optional User qq.
+@apiSuccess {String} user.gender Optional User gender.
 
 @apiError UserNotFound The <code>id</code> of the user was not found.
+
+@apiVersion 0.0.0
+"""
+
+"""
+@api {post} /user/register/ User Register
+@apiName UserRegister
+@apiDescription Handle user registration request.
+@apiGroup User
+
+@apiParam {String} email Mandatory email address.
+@apiParam {String} password Mandatory password.
+@apiParam {String} password_confirmation Mandatory confirmation password.
+@apiParam {String} name Mandatory name.
+@apiParam {String} address Mandatory address.
+@apiParam {Number} tel Mandatory phone number.
+
+@apiSuccess {Number} id Registered user id.
+
+@apiError EmailAlreadyExists The email exists in the DB.
+@apiError PasswordDoesNotMath The password does not match password_confirmation.
+@apiError RequiredFieldBlank Any mandatory form is blank.
 
 @apiVersion 0.0.0
 """

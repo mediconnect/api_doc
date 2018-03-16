dfc25673c7a694ee461560bcb3d0274c1d3f7177
unknown <runye.wang@emory.edu>
2018-03-16 13:21:47 -0400

    supervisor api calls except questionnaire related

--- api/slot.py
+++ api/slot.py
@@ -11,3 +11,19 @@
 
 @apiError SlotNotFound The combination of <code>hospital_id</code> and <code>disease_id</code> of the Slot was not found.
 """
+
+"""
+@api {put} /slot/:hospital_id/:disease_id Update Slot Information
+@apiName UpdateSlot
+@apiGroup Slot
+@apiDescription Update a slot.
+
+@apiParam {Number} hospital_id			Hospital ID
+@apiParam {Number} disease_id			Disease ID
+@apiParam {Number[4]} available 		Abailable slots for the next four weeks.
+@apiParam {String} 	token 				Token to verify authorization.
+
+@apiError SlotNotFound The combination of <code>hospital_id</code> and <code>disease_id</code> of the Slot was not found.
+@apiError InvalidAuthorization		This operation is invalid at this stage.
+
+"""
--- api/supervisor.py
+++ api/supervisor.py
@@ -22,7 +22,7 @@
 """
 
 """
-@api {get} /reservations/:query Request list of Reservations
+@api {get} /supervisor/reservations/:query Request list of Reservations
 @apiName ListReservation
 @apiGroup Supervisor
 @apiDescription Request a list of reservation based on query.
@@ -32,6 +32,21 @@
 @apiSuccess {Number}	page					Pagination.
 @apiSuccess {Number}	total_page				Total pages.
 @apiSuccess {Object[]} 	reservations 			List of filterd/sorted reservations.
+
+
+@apiVersion 0.0.0
+"""
+
+
+"""
+@api {get} /supervisor/reservation/:id Request info of a Reservation
+@apiName ReservationInfo
+@apiGroup Supervisor
+@apiDescription Request the informaiton of a Reservation.
+
+@apiParam {Number} id 	Unique ID of a Reservation.
+
+@apiSuccess {Object}	reservation 			Reservation Object.
 @apiSuccess {Number} 	reservation.id			Reservation unique id.
 @apiSuccess	{String}	reservation.customer.name	Customer name.
 @apiSuccess {String} 	reservaiton.reservation_patient.name Reservation_patient name.
@@ -44,10 +59,56 @@
 @apiSuccess {DateTime}	reservation.deadline 	Reservation deadline to upload to hospital.
 @apiSuccess {DateTime}	reservation.trans_deadline 	Reservation translating deadline. 
 @apiSuccess {DateTime} 	reservation.last_upload 	Last upload DateTime of documents associated.
-@apiSuccess {String}	reservation.url 		Link to reservation detail. 
 @apiSuccess {Boolean} 	reservation.re_assigned 	Whether reservation has been reassigned.
 
-@apiError ReservationNotFound The required reservations based on <code>query</code> are not found. 
+@apiError ReservationNotFound The required <code>id</code> of reservation is not found.
+
+@apiVersion 0.0.0
+"""
+
+"""
+@api {put} /supervisor/reservation/:id Update a Reservation
+@apiName Update Reservation
+@apiGroup Supervisor
+@apiDescription Update the informaiton of a Reservation.
+
+@apiParam {Number} 	id 							Unique ID of a Reservation.
+@apiParam {Number} 	translator_C2E.id 			New C2E_Translator id.
+@apiParam {Number} 	translator_E2C.id 			New E2C_Translaotr id.
+@apiParam {Number}	status						New reservation status.
+@apiParam {Number} 	trans_status 				New translating status.
+@apiParam {String} 	token 						Token to verify authorization.
+
+@apiError ReservationNotFound 		The required <code>id</code> of reservation is not found.
+@apiError InvalidAuthorization 			The operation is invalid at this stage. 
+
+@apiVersion 0.0.0
+"""
+
+"""
+@api {delete} /supervisor/reservation/:id Delete a Reservation
+@apiName Delete Reservation
+@apiGroup Supervisor
+@apiDescription Remove a Reservation from Database.
+
+@apiParam {Number} 	id 				Unique ID of a Reservation.
+@apiParam {String} 	token 			Token to verify authorization.
+
+@apiError InvalidAuthorization		This operation is invalid at this stage.
+"""
+
+"""
+@api {get} /supervisor/reservation/documents/:id/:type Request Documents associated with a Reservation
+@apiName ReservationDocuments
+@apiGroup Supervisor
+@apiDescription Request the documents associated with a reservation. 
+
+@apiParam {Number} id 	Reservation unique ID.
+@apiParam {String} type Document type
+
+@apiSuccess {Object[]} documents 	List of Document Objects
+
+@apiError ReservationNotFound The required <code>id</code> of reservation is not found.
 
 @apiVersion 0.0.0
 """

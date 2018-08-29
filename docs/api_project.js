2bd2c1be582ad12ff50d1fd9635e4444220aa30d
Arnoldosmium <eaglemewpsy@gmail.com>
2018-08-28 23:02:59 -0400

    refreshing docs

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-08-29T03:02:52.040Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});

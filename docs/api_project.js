b864e660607370c7f7be6a017847875ddcdaab66
gabrielwry <me@gabrielwry.com>
2018-11-08 20:45:00 -0500

    rename to custoemr-patient

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-11-09T01:12:35.764Z",
+    "time": "2018-11-09T01:44:10.585Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }

9dae4d95180a84c8ca0c256eb8922b9fadcfc609
gabrielwry <me@gabrielwry.com>
2018-11-11 09:45:32 -0500

    customer profile api fix

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-11-09T05:36:25.938Z",
+    "time": "2018-11-11T14:45:04.891Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }

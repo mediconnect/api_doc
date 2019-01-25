aab9af5fdb5f810962c1f35f072164047aa6645e
Gabrielwry <me@gabrielwry.com>
2019-01-24 21:37:31 -0500

    quick fix on reservation admin api doc (#17)
    * quick fix on reservation admin api doc
    
    * fix typo

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,1 +1,14 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-01-24T07:36:13.830Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-01-24T13:18:53.864Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});

ee2f5706c01819953649005acc5ebd1c14e4d8f9
Fei Gao <fgao22@live.com>
2019-01-28 22:06:57 +0800

    Fix /customer (#20)
    * change customer yml
    
    * compile
    
    * .

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,14 +1,1 @@
-define({
-  "name": "",
-  "version": "0.0.0",
-  "description": "",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-01-28T04:12:49.952Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
-  }
-});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-01-28T14:03:43.472Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});

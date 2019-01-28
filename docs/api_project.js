fa63eb5a06e13c4d583efacad5bcd0823515a974
Jiashen Cao <caojiashen24@gmail.com>
2019-01-27 21:25:14 -0500

    update contact us (#21)

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,14 +1,1 @@
-define({
-  "name": "",
-  "version": "0.0.0",
-  "description": "",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-01-26T20:31:07.081Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
-  }
-});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-01-27T22:13:40.772Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});

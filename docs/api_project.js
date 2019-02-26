3f348accba8340bc83bc5526786bb56f41e8a344
Arnoldosmium <eaglemewpsy@gmail.com>
2019-02-26 09:14:41 -0500

    update doc html

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-02-18T02:09:53.389Z",
+    "time": "2019-02-26T14:14:36.446Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }

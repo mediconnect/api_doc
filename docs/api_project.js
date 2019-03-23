069ccd98056e1f81b110d9291a83a1900d84e652
Fei Gao <fgao22@live.com>
2019-03-23 13:53:00 +0000

    Contact Us (#31)
    * change
    
    * compile
    
    * revert

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,14 +1,1 @@
-define({
-  "name": "",
-  "version": "0.0.0",
-  "description": "",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2019-02-27T06:05:21.927Z",
-    "url": "http://apidocjs.com",
-    "version": "0.17.6"
-  }
-});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-03-23T13:37:51.570Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
